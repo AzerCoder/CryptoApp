34a8f1ecda65031fbb7f8012a109e6f3edb6ea08
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-07-17 15:12:13 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by A'zamjon Abdumuxtorov on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
